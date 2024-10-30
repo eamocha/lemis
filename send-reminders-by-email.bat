d5bc37ff910141cba999693efd0b718b0406a8eb
eamocha <eamocha@gmail.com>
2024-10-30 17:04:41 +0300

    first upload

---
+++ send-reminders-by-email.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\php\php7.2\php.exe" -f "C:\inetpub\wwwroot\kebs\index.php" cron_jobs send_emails_before_reminders_due_dates
---
+++ send-sla-breach-by-email.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\php\php7.2\php.exe" -f "C:\inetpub\wwwroot\kebs\index.php" cron_jobs send_emails_for_breached_slas
